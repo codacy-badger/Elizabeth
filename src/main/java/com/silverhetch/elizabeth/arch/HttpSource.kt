5344e68ccf9d9d3d0f45d01e6334c8bb60bb93d3
Larry Hsiao <larryhsiao@silverhetch.com>
2018-05-06 20:01:52 +0800

    using interface instead of class as parameter

--- src/main/java/com/silverhetch/elizabeth/arch/HttpSource.kt
+++ src/main/java/com/silverhetch/elizabeth/arch/HttpSource.kt
@@ -1,9 +1,8 @@
 package com.silverhetch.elizabeth.arch
 
-import com.silverhetch.clotho.connection.WebApi
-import com.silverhetch.elizabeth.arch.Source
+import com.silverhetch.clotho.connection.WebRequest
 
-class HttpSource(private val api: WebApi) : Source<String> {
+class HttpSource(private val api: WebRequest) : Source<String> {
     override fun fetch(): String {
         return String(api.request().bodyBytes)
     }
