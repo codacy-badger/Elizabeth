9c86c5060753ab1fec7889a5e4e9aa6c9d659f4b
Larry Hsiao <larryhsiao@silverhetch.com>
2018-05-07 17:54:30 +0800

    postgres connection test using isValid method

--- src/test/java/com/silverhetch/elizabeth/database/postgres/PostgresConnectionTest.kt
+++ src/test/java/com/silverhetch/elizabeth/database/postgres/PostgresConnectionTest.kt
@@ -8,9 +8,7 @@ class PostgresConnectionTest {
     @Test
     fun connection() {
         PostgreSQLConnection(ElizabethConfig()).instance().use { conn ->
-            conn.createStatement().executeQuery("SELECT * from testing;").use{result ->
-                Assert.assertEquals(0,result.fetchSize)
-            }
+            Assert.assertTrue(conn.isValid(10))
         }
     }
 }
